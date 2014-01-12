16ea4aec6feef1f85a72a95f536246a0d3e5fd5a
Martin Pozor <martin.pozor@pozyx.com>
2014-01-12 18:22:00 +0000

    BoolArrayCellSpace done

--- Pozyx.CAE/Pozyx.CAE.Lib/BoolArrayCellSpace.cs
+++ Pozyx.CAE/Pozyx.CAE.Lib/BoolArrayCellSpace.cs
@@ -4,22 +4,23 @@ using System.IO;
 
 namespace Pozyx.CAE.Lib
 {
-    public struct BitArrayCellSpace : ICellSpace
+    public struct BoolArrayCellSpace : ICellSpace
     {        
-        private BitArray _bitArray;
-
+        private bool[] _bools;
+        
         public int Offset { get; private set; }
 
         public int Length
         {
-            get { return _bitArray.Length; }
-        }
+            get { return _bools.Length; }
+        }        
 
         public void Initialize(BitArray bitArray, int offset)
         {
-            _bitArray = bitArray;
+            _bools = new bool[bitArray.Length];
+            ((ICollection) bitArray).CopyTo(_bools, 0);            
             Offset = offset;
-        }
+        }        
 
         public bool Get(int index)
         {
@@ -27,43 +28,43 @@ namespace Pozyx.CAE.Lib
 
             return
                 index >= 0 &&
-                index < _bitArray.Length &&
-                _bitArray.Get(index);
-        }
+                index < _bools.Length &&
+                _bools[index];
+        }        
 
         public void Set(int index, bool value)
         {
             index -= Offset;
 
-            if (index < 0 || index >= _bitArray.Length)
+            if (index < 0 || index >= _bools.Length)
                 throw new Exception("Invalid index to write");
 
-            _bitArray.Set(index, value);
+            _bools[index] = value;
         }
 
         public string ToCellString(long? spaceStart = null)
-        {            
+        {
             if (!spaceStart.HasValue)
                 spaceStart = Offset;
 
             if (spaceStart > Offset)
-                throw new ArgumentException("Space start cannot be higher than offset.", "spaceStart");            
+                throw new ArgumentException("Space start cannot be higher than offset.", "spaceStart");
 
             using (var sw = new StringWriter())
             {
-                for (var i = spaceStart.Value; i < Offset; i++)                
-                    sw.Write(' ');                
+                for (var i = spaceStart.Value; i < Offset; i++)
+                    sw.Write(' ');
 
-                for (var i = 0; i < _bitArray.Length; i++)
+                for (var i = 0; i < _bools.Length; i++)
                 {
                     //if ((i != 0) && (i % (sizeof(int) * 8) == 0))
                     //    sw.Write(" ");
 
-                    sw.Write(_bitArray[i] ? '█' : ' ');
+                    sw.Write(_bools[i] ? '█' : ' ');
                 }
 
                 return sw.ToString();
             }
-        }                
+        }        
     }
 }
